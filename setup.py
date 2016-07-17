c6e2d4ed3c820174c3ab7bb2cd56cbd098e13ac6
Stephane Caron <stephane.caron@normalesup.org>
2016-07-17 17:17:46 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='oqp',
+    version='0.1.0',
+    description="Wrapper around QP solvers in Python",
+    url="https://github.com/stephane-caron/oqp",
+    author="Stéphane Caron",
+    author_email="stephane.caron@normalesup.org",
+    license="GPL",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 2.7'],
+    packages=['oqp']
+)
