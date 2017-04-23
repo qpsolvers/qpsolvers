b16d3ebc5b21f8eba6e978596767d6d166397eb0
Stéphane Caron <stephane.caron@normalesup.org>
2017-04-23 19:17:21 +0200

    Added equality constraints to quadprog wrapper

--- qpsolvers/quadprog_.py
+++ qpsolvers/quadprog_.py
@@ -50,12 +50,21 @@ def quadprog_solve_qp(P, q, G=None, h=None, A=None, b=None, initvals=None):
     b : array, shape=(meq,), optional
         Linear equality constraint vector.
     initvals : array, shape=(n,), optional
-        Warm-start guess vector.
+        Warm-start guess vector (not used).
 
     Returns
     -------
     x : array, shape=(n,)
         Solution to the QP, if found, otherwise ``None``.
     """
-    P = .5 * (P + P.T)  # quadprog assumes that P is symmetric
-    return solve_qp(P, -q, -G.T, -h)[0]
+    qp_G = .5 * (P + P.T)   # quadprog assumes that P is symmetric
+    qp_a = -q
+    if A is not None:
+        qp_C = -vstack([A, G]).T
+        qp_b = -hstack([b, h])
+        meq = A.shape[0]
+    else:  # no equality constraint
+        qp_C = -G.T
+        qp_b = -h
+        meq = 0
+    return solve_qp(qp_G, qp_a, qp_C, qp_b, meq)[0]
