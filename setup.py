e05488aa1c028ed5412087975d7c2abdc96c4acb
Stéphane Caron <stephane.caron@normalesup.org>
2017-11-12 13:44:41 +0100

    License changed in setup.py as well

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016-2017 Stephane Caron <stephane.caron@normalesup.org>
+#
+# This file is part of qpsolvers.
+#
+# qpsolvers is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# qpsolvers is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with qpsolvers. If not, see <http://www.gnu.org/licenses/>.
 
 from distutils.core import setup
 
@@ -15,7 +33,7 @@ setup(
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Mathematics',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'License :: OSI Approved :: GNU Lesser General Public License (LGPL)',
         'Programming Language :: Python :: 2.7'],
     packages=['qpsolvers']
 )
