3d5c44f1849eb1c8093b749275e3d7b5d2acf79a
Stéphane Caron <stephane.caron@normalesup.org>
2017-11-12 13:33:44 +0100

    Updated license notices in each Python file

--- qpsolvers/qpoases_.py
+++ qpsolvers/qpoases_.py
@@ -5,18 +5,18 @@
 #
 # This file is part of qpsolvers.
 #
-# qpsolvers is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation, either version 3 of the License, or (at your option) any later
-# version.
+# qpsolvers is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
 #
-# qpsolvers is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# qpsolvers is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
 # details.
 #
-# You should have received a copy of the GNU General Public License along with
-# qpsolvers. If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Lesser General Public License along
+# with qpsolvers. If not, see <http://www.gnu.org/licenses/>.
 
 from numpy import array, hstack, ones, vstack, zeros
 from qpoases import PyOptions as Options
