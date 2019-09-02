f4f34015df0b4a913a8b19af6138302f380267f6
Stéphane Caron <stephane.caron@lirmm.fr>
2019-09-02 11:13:24 +0200

    [quadprog] Handle case where A.ndim == 1

--- qpsolvers/quadprog_.py
+++ qpsolvers/quadprog_.py
@@ -69,13 +69,15 @@ def quadprog_solve_qp(P, q, G=None, h=None, A=None, b=None, initvals=None):
     qp_G = P
     qp_a = -q
     if A is not None:
+        if A.ndim == 1:
+            A = A.reshape((1, A.shape[0]))
         if G is None:
             qp_C = -A.T
             qp_b = -b
         else:
             qp_C = -vstack([A, G]).T
             qp_b = -hstack([b, h])
-        meq = A.shape[0] if A.ndim > 1 else 1
+        meq = A.shape[0]
     else:  # no equality constraint
         qp_C = -G.T if G is not None else None
         qp_b = -h if h is not None else None
