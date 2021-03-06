8fcbe3a53285059c698c1bf68ca7742599274b24
Dustin Kenefake <Dustin.Kenefake@tamu.edu>
2021-03-06 15:28:19 -0600

    Update gurobi_.py
    Updated to Gurobi 9.0 matrix API.

--- qpsolvers/gurobi_.py
+++ qpsolvers/gurobi_.py
@@ -18,17 +18,9 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with qpsolvers. If not, see <http://www.gnu.org/licenses/>.
 
-from numpy import empty
 from gurobipy import GRB
 import gurobipy as gp
 from typing import Optional
-
-
-try:
-    setParam('OutputFlag', 0)
-except GurobiError as e:
-    print("GurobiError: {}".format(e))
-
 
 
 def gurobi_solve_qp(P, q, G = None, h = None, A = None, b = None, initvals = None, verbose: bool = False) -> Optional[numpy.ndarray]:
