50fe2a83e612b4e379745a96cd65f50485c32943
Stéphane Caron <stephane.caron@normalesup.org>
2021-04-24 13:08:12 +0200

    [doc] Read version info from __init__.py directly

--- qpsolvers/__init__.py
+++ qpsolvers/__init__.py
@@ -24,7 +24,7 @@ from typing import Any, Dict, Optional
 
 from numpy import concatenate, dot, eye, hstack, ones, ndarray, vstack, zeros
 
-__version__ = "1.7"
+__version__ = "1.7.0"
 
 available_solvers = []
 dense_solvers = []
