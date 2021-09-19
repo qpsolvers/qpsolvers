b076ab6f93b8f7fcd6ecbe5822ce74fa798c3caf
Stéphane Caron <stephane.caron@normalesup.org>
2021-09-19 11:35:44 +0200

    Add unit tests for least-squares

---
+++ tests/test_solve_ls.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016-2021 Stéphane Caron <stephane.caron@normalesup.org>
+#
+# This file is part of qpsolvers.
+#
+# qpsolvers is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# qpsolvers is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with qpsolvers. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Tests for the main `solve_qp` function.
+"""
+
+import unittest
+import warnings
+
+from numpy import allclose, array, dot
+from numpy.linalg import norm
+from qpsolvers import available_solvers, solve_ls
+
+
+class TestSolveLS(unittest.TestCase):
+
+    """
+    Test fixture for the README example problem.
+    """
+
+    def setUp(self):
+        """
+        Prepare test fixture.
+        """
+        warnings.simplefilter("ignore", category=DeprecationWarning)
+        warnings.simplefilter("ignore", category=UserWarning)
+        self.R = array([[1.0, 2.0, 0.0], [-8.0, 3.0, 2.0], [0.0, 1.0, 1.0]])
+        self.s = array([3.0, 2.0, 3.0])
+        self.G = array([[1.0, 2.0, 1.0], [2.0, 0.0, 1.0], [-1.0, 2.0, -1.0]])
+        self.h = array([3.0, 2.0, -2.0]).reshape((3,))
+        self.A = array([1.0, 1.0, 1.0])
+        self.b = array([1.0])
+
+    def get_problem(self):
+        """
+        Get problem as a sextuple of values to unpack.
+
+        Returns
+        -------
+        R : numpy.ndarray
+            Least-squares matrix .
+        s : numpy.ndarray
+            Least-squares vector.
+        G : numpy.ndarray
+            Linear inequality matrix.
+        h : numpy.ndarray
+            Linear inequality vector.
+        A : numpy.ndarray, scipy.sparse.csc_matrix or cvxopt.spmatrix
+            Linear equality matrix.
+        b : numpy.ndarray
+            Linear equality vector.
+        """
+        return self.R, self.s, self.G, self.h, self.A, self.b
+
+    @staticmethod
+    def get_test(solver):
+        """
+        Get test function for a given solver.
+
+        Parameters
+        ----------
+        solver : string
+            Name of the solver to test.
+
+        Returns
+        -------
+        test : function
+            Test function for that solver.
+        """
+
+        def test(self):
+            R, s, G, h, A, b = self.get_problem()
+            x = solve_ls(R, s, G, h, A, b, solver=solver)
+            self.assertIsNotNone(x)
+            known_solution = array([-0.01633987, -0.33333333, 1.3496732])
+            sol_tolerance = 1e-5 if solver == "ecos" else 1e-8
+            self.assertTrue(norm(x - known_solution) < sol_tolerance)
+            self.assertTrue(max(dot(G, x) - h) <= 1e-10)
+            self.assertTrue(allclose(dot(A, x), b))
+
+        return test
+
+
+# Generate test fixtures for each solver
+for solver in available_solvers:
+    setattr(
+        TestSolveLS, "test_{}".format(solver), TestSolveLS.get_test(solver)
+    )
+
+
+if __name__ == "__main__":
+    unittest.main()
