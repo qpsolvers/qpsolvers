a9aa9fe6429dabaf7d0ffbce0667733807b21a80
Stéphane Caron <stephane.caron@normalesup.org>
2022-01-12 15:54:03 +0100

    Use assertLess to check tolerances
    Error reports are more informative than with assertTrue

--- qpsolvers/utils.py
+++ qpsolvers/utils.py
@@ -18,6 +18,8 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with qpsolvers. If not, see <http://www.gnu.org/licenses/>.
 
+"""Utility functions"""
+
 from numpy import ndarray
 from scipy.sparse import csc_matrix
 
