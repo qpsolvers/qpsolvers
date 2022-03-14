da4fbf0d4c9a770ecac548f031549012aa6bc759
Dustin Kenefake <Dustin.Kenefake@tamu.edu>
2022-03-14 12:32:04 -0500

    Update qpsolvers/solver_output.py
    Co-authored-by: Stéphane Caron <stephane@scaron.info>

--- qpsolvers/solver_output.py
+++ qpsolvers/solver_output.py
@@ -1,3 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016-2022 The qpsolvers contributors.
+#
+# This file is part of qpsolvers.
+#
+# qpsolvers is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# qpsolvers is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with qpsolvers. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Type for extended QP solver outputs going beyond the primal solution.
+"""
+
 from dataclasses import dataclass
 from typing import Optional
 
