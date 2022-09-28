0b0f6af2a949bf2922694c26de9e16c310fd5e19
Stéphane Caron <stephane.caron@normalesup.org>
2022-09-28 19:04:52 +0200

    Print expected solution in LS example

--- examples/least_squares.py
+++ examples/least_squares.py
@@ -22,19 +22,24 @@
 Test the "quadprog" QP solver on a small dense problem.
 """
 
+import random
+from time import perf_counter
+
 import numpy as np
 
-from qpsolvers import solve_ls, print_matrix_vector
-from time import perf_counter
+import qpsolvers
+from qpsolvers import print_matrix_vector, solve_ls
 
 R = np.array([[1.0, 2.0, 0.0], [-8.0, 3.0, 2.0], [0.0, 1.0, 1.0]])
 s = np.array([3.0, 2.0, 3.0])
 G = np.array([[1.0, 2.0, 1.0], [2.0, 0.0, 1.0], [-1.0, 2.0, -1.0]])
 h = np.array([3.0, 2.0, -2.0])
 
+x_sol = np.array([0.1299734765610818, -0.0649867382805409, 1.7400530468778364])
+
 if __name__ == "__main__":
     start_time = perf_counter()
-    solver = "quadprog"  # see qpsolvers.available_solvers
+    solver = random.choice(qpsolvers.available_solvers)
     x = solve_ls(R, s, G, h, solver=solver, verbose=True)
     end_time = perf_counter()
 
@@ -47,5 +52,6 @@ if __name__ == "__main__":
     print_matrix_vector(G, "G", h, "h")
     print("")
     print(f"Solution: x = {x}")
+    print(f"It should be close to x* = {x_sol}")
     print(f"Solve time: {1e6 * (end_time - start_time):.0f} [us]")
     print(f"Solver: {solver}")
