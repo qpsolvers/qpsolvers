cd789f21b183d19107102bbae31954e5f42a0102
Stéphane Caron <stephane.caron@normalesup.org>
2022-11-04 10:34:51 +0100

    Remove CVXOPT exn catching
    It shouldn't be necessary any more, and used to hide other import errors
    within the try block (see previous commit).

--- tests/test_cvxopt.py
+++ tests/test_cvxopt.py
@@ -22,112 +22,106 @@ import unittest
 import warnings
 from typing import Tuple
 
+import cvxopt
 import numpy as np
 import scipy
+from cvxopt import matrix as dense_matrix
 from numpy import array, ones
 from numpy.linalg import norm
+from scipy.sparse import csc_matrix
+
 from qpsolvers import solve_qp
 from qpsolvers.solvers.conversions import linear_from_box_inequalities
-from scipy.sparse import csc_matrix
+from qpsolvers.solvers.cvxopt_ import to_cvxopt
 
 from .problems import get_sd3310_problem
 
-try:
-    import cvxopt
-    from cvxopt import matrix as dense_matrix
-    from qpsolvers.solvers.cvxopt_ import to_cvxopt
+class TestCVXOPT(unittest.TestCase):
 
-    class TestCVXOPT(unittest.TestCase):
+    """
+    Test fixture for the CVXOPT solver.
+    """
 
+    def setUp(self):
         """
-        Test fixture for the CVXOPT solver.
+        Prepare test fixture.
         """
+        warnings.simplefilter("ignore", category=UserWarning)
 
-        def setUp(self):
-            """
-            Prepare test fixture.
-            """
-            warnings.simplefilter("ignore", category=UserWarning)
+    def get_sparse_problem(
+        self,
+    ) -> Tuple[cvxopt.matrix, np.ndarray, cvxopt.matrix, np.ndarray]:
+        """
+        Get sparse problem as a quadruplet of values to unpack.
 
-        def get_sparse_problem(
-            self,
-        ) -> Tuple[cvxopt.matrix, np.ndarray, cvxopt.matrix, np.ndarray]:
-            """
-            Get sparse problem as a quadruplet of values to unpack.
+        Returns
+        -------
+        P :
+            Symmetric quadratic-cost matrix .
+        q :
+            Quadratic-cost vector.
+        G :
+            Linear inequality matrix.
+        h :
+            Linear inequality vector.
+        """
+        n = 150
+        M = scipy.sparse.lil_matrix(scipy.sparse.eye(n))
+        for i in range(1, n - 1):
+            M[i, i + 1] = -1
+            M[i, i - 1] = 1
+        P = to_cvxopt(csc_matrix(M.dot(M.transpose())))
+        q = -ones((n,))
+        G = to_cvxopt(csc_matrix(-scipy.sparse.eye(n)))
+        h = -2.0 * ones((n,))
+        return P, q, G, h
 
-            Returns
-            -------
-            P :
-                Symmetric quadratic-cost matrix .
-            q :
-                Quadratic-cost vector.
-            G :
-                Linear inequality matrix.
-            h :
-                Linear inequality vector.
-            """
-            n = 150
-            M = scipy.sparse.lil_matrix(scipy.sparse.eye(n))
-            for i in range(1, n - 1):
-                M[i, i + 1] = -1
-                M[i, i - 1] = 1
-            P = to_cvxopt(csc_matrix(M.dot(M.transpose())))
-            q = -ones((n,))
-            G = to_cvxopt(csc_matrix(-scipy.sparse.eye(n)))
-            h = -2.0 * ones((n,))
-            return P, q, G, h
+    def test_sparse(self):
+        """
+        Test CVXOPT on a sparse problem.
+        """
+        P, q, G, h = self.get_sparse_problem()
+        x = solve_qp(P, q, G, h, solver="cvxopt")
+        self.assertIsNotNone(x)
+        known_solution = array([2.0] * 149 + [3.0])
+        sol_tolerance = 1e-2  # aouch, not great!
+        h_cvxopt, x_cvxopt = to_cvxopt(h), to_cvxopt(x)
+        self.assertLess(norm(x - known_solution), sol_tolerance)
+        self.assertLess(max(G * x_cvxopt - h_cvxopt), 1e-10)
 
-        def test_sparse(self):
-            """
-            Test CVXOPT on a sparse problem.
-            """
-            P, q, G, h = self.get_sparse_problem()
-            x = solve_qp(P, q, G, h, solver="cvxopt")
-            self.assertIsNotNone(x)
-            known_solution = array([2.0] * 149 + [3.0])
-            sol_tolerance = 1e-2  # aouch, not great!
-            h_cvxopt, x_cvxopt = to_cvxopt(h), to_cvxopt(x)
-            self.assertLess(norm(x - known_solution), sol_tolerance)
-            self.assertLess(max(G * x_cvxopt - h_cvxopt), 1e-10)
+    def test_concatenate_bounds(self):
+        """
+        Concatenate bounds with a CVXOPT inequality matrix.
+        """
+        _, _, G, h = self.get_sparse_problem()
+        lb = -np.ones(G.size[1])
+        ub = +np.ones(G.size[1])
+        G2, h2 = linear_from_box_inequalities(G, h, lb, ub)
+        G2 = np.array(dense_matrix(G2))
+        m = G.size[0]
+        n = lb.shape[0]
+        self.assertTrue(np.allclose(G2[m : m + n, :], -np.eye(n)))
+        self.assertTrue(np.allclose(h2[m : m + n], -lb))
+        self.assertTrue(np.allclose(G2[m + n : m + 2 * n, :], np.eye(n)))
+        self.assertTrue(np.allclose(h2[m + n : m + 2 * n], ub))
 
-        def test_concatenate_bounds(self):
-            """
-            Concatenate bounds with a CVXOPT inequality matrix.
-            """
-            _, _, G, h = self.get_sparse_problem()
-            lb = -np.ones(G.size[1])
-            ub = +np.ones(G.size[1])
-            G2, h2 = linear_from_box_inequalities(G, h, lb, ub)
-            G2 = np.array(dense_matrix(G2))
-            m = G.size[0]
-            n = lb.shape[0]
-            self.assertTrue(np.allclose(G2[m : m + n, :], -np.eye(n)))
-            self.assertTrue(np.allclose(h2[m : m + n], -lb))
-            self.assertTrue(np.allclose(G2[m + n : m + 2 * n, :], np.eye(n)))
-            self.assertTrue(np.allclose(h2[m + n : m + 2 * n], ub))
-
-        def test_extra_kwargs(self):
-            """
-            Call CVXOPT with various solver-specific settings.
-            """
-            P, q, G, h, A, b = get_sd3310_problem()
-            x = solve_qp(
-                P,
-                q,
-                G,
-                h,
-                A,
-                b,
-                solver="cvxopt",
-                maxiters=10,
-                abstol=1e-1,
-                reltol=1e-1,
-                feastol=1e-2,
-                refinement=3,
-            )
-            self.assertIsNotNone(x)
-
-
-except ImportError:  # CVXOPT is not installed
-
-    pass
+    def test_extra_kwargs(self):
+        """
+        Call CVXOPT with various solver-specific settings.
+        """
+        P, q, G, h, A, b = get_sd3310_problem()
+        x = solve_qp(
+            P,
+            q,
+            G,
+            h,
+            A,
+            b,
+            solver="cvxopt",
+            maxiters=10,
+            abstol=1e-1,
+            reltol=1e-1,
+            feastol=1e-2,
+            refinement=3,
+        )
+        self.assertIsNotNone(x)
