5eb53c1e59959b22524695c9515e44eaf40414bb
Stéphane Caron <stephane.caron@normalesup.org>
2022-11-07 09:27:09 +0100

    [qpoases] Add unit tests specific to qpOASES

---
+++ tests/test_qpoases.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016-2022 Stéphane Caron and the qpsolvers contributors.
+#
+# This file is part of qpsolvers.
+#
+# qpsolvers is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# qpsolvers is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with qpsolvers. If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+import numpy as np
+
+from qpsolvers import solve_qp
+
+from .problems import get_sd3310_problem
+
+try:
+    import qpoases
+
+    class TestQpOASES(unittest.TestCase):
+
+        """
+        Test fixture specific to the qpOASES solver.
+        """
+
+        def setUp(self):
+            self.assertTrue(hasattr(qpoases, "PyQProblem"))
+
+        def test_initvals(self):
+            """
+            Call the solver with a warm-start guess.
+            """
+            P, q, G, h, A, b = get_sd3310_problem()
+            solve_qp(
+                P,
+                q,
+                G,
+                h,
+                A,
+                b,
+                solver="qpoases",
+                initvals=q,
+            )
+
+        def test_time_limit(self):
+            """
+            Call the solver with a time limit.
+            """
+            P, q, G, h, A, b = get_sd3310_problem()
+            solve_qp(
+                P,
+                q,
+                G,
+                h,
+                A,
+                b,
+                solver="qpoases",
+                time_limit=0.1,
+            )
+            solve_qp(
+                P,
+                q,
+                solver="qpoases",
+                time_limit=0.1,
+            )
+
+        def test_unfeasible(self):
+            P, q, G, h, A, b = get_sd3310_problem()
+            lb = np.ones(q.shape)
+            ub = -np.ones(q.shape)
+            x = solve_qp(
+                P,
+                q,
+                G,
+                h,
+                A,
+                b,
+                lb,
+                ub,
+                solver="qpoases",
+            )
+            self.assertIsNone(x)
+
+
+except ImportError:  # qpOASES not installed
+
+    pass
