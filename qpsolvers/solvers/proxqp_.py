548120d269eb4db51dcc4732cc761b4e7edc6f23
Stéphane Caron <stephane.caron@normalesup.org>
2022-11-14 11:38:28 +0100

    [proxqp] Also consider G and A to select backend

--- qpsolvers/solvers/proxqp_.py
+++ qpsolvers/solvers/proxqp_.py
@@ -223,7 +223,11 @@ def proxqp_solve_qp(
             )
         kwargs["x"] = initvals
     n: int = q.shape[0]
-    use_csc: bool = isinstance(P, (spa.csc_matrix, spa.dia_matrix))
+    use_csc: bool = (
+        not isinstance(P, np.ndarray)
+        or (G is not None and not isinstance(G, np.ndarray))
+        or (A is not None and not isinstance(A, np.ndarray))
+    )
     C_prox, u_prox, l_prox = __combine_inequalities(G, h, lb, ub, n, use_csc)
     solve_function = __select_backend(backend, use_csc)
     result = solve_function(
