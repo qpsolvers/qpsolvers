2f6e97a19f7f58702b555f91f403432408505702
Stéphane Caron <stephane.caron@normalesup.org>
2023-01-03 15:58:13 +0100

    Remove solve_safer_qp

--- qpsolvers/__init__.py
+++ qpsolvers/__init__.py
@@ -25,7 +25,6 @@ from .problem import Problem
 from .solution import Solution
 from .solve_ls import solve_ls
 from .solve_qp import solve_problem, solve_qp
-from .solve_safer_qp import solve_safer_qp
 from .solvers import (
     available_solvers,
     cvxopt_solve_qp,
@@ -68,6 +67,5 @@ __all__ = [
     "solve_ls",
     "solve_problem",
     "solve_qp",
-    "solve_safer_qp",
     "sparse_solvers",
 ]
