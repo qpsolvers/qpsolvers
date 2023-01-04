93d21a1219053cc811474448cafc8d9465f0e4f8
Stéphane Caron <stephane.caron@normalesup.org>
2023-01-04 19:19:33 +0100

    Add unit test that catches a current bug

--- tests/test_solution.py
+++ tests/test_solution.py
@@ -20,6 +20,7 @@
 
 import unittest
 
+import numpy as np
 from qpsolvers import Solution, solve_problem
 
 from .problems import get_sd3310_problem
@@ -43,3 +44,29 @@ class TestSolution(unittest.TestCase):
         self.assertLess(solution.dual_residual(), eps_abs)
         self.assertLess(solution.duality_gap(), eps_abs)
         self.assertTrue(solution.is_optimal(eps_abs))
+
+    def test_undefined_optimality(self):
+        solution = Solution(get_sd3310_problem())
+
+        # solution is fully undefined
+        self.assertEqual(solution.primal_residual(), np.inf)
+        self.assertEqual(solution.dual_residual(), np.inf)
+        self.assertEqual(solution.duality_gap(), np.inf)
+
+        solution.x = np.array([1.0, 2.0, 3.0])
+        self.assertNotEqual(solution.primal_residual(), np.inf)
+        self.assertEqual(solution.dual_residual(), np.inf)
+        self.assertEqual(solution.duality_gap(), np.inf)
+
+        solution.z = np.array([-1.0, -2.0, -3.0])
+        self.assertEqual(solution.dual_residual(), np.inf)
+        self.assertEqual(solution.duality_gap(), np.inf)
+
+        solution.y = np.array([0.0])
+        self.assertNotEqual(solution.dual_residual(), np.inf)
+        self.assertNotEqual(solution.duality_gap(), np.inf)
+
+        # solution is now fully defined
+        self.assertGreater(solution.primal_residual(), 10.0)
+        self.assertGreater(solution.dual_residual(), 10.0)
+        self.assertGreater(solution.duality_gap(), 10.0)
