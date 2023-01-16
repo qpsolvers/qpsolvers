2efde525f8c03327e328ec91d3b29d524815f69a
Stéphane Caron <stephane.caron@normalesup.org>
2023-01-16 09:57:10 +0100

    [CI] Group unit tests for solve_problem

--- tests/test_solve_problem.py
+++ tests/test_solve_problem.py
@@ -22,20 +22,141 @@
 Tests for the `solve_problem` function.
 """
 
+import math
 import unittest
 
 from numpy.linalg import norm
 
 from qpsolvers import available_solvers, solve_problem
 
-from .solved_problems import get_maros_meszaros_qptest
+from .solved_problems import (
+    get_maros_meszaros_qptest,
+    get_qpsut01,
+    get_qpsut02,
+    get_qpsut03,
+)
 
 
 class TestSolveProblem(unittest.TestCase):
 
     """
-    Test fixture for a variety of quadratic programs.
+    Test fixture for primal and dual solutions of a variety of quadratic
+    programs.
+
+    Notes
+    -----
+    Solver-specific tests are implemented in static methods called
+    ``get_test_{foo}`` that return the test function for a given solver. The
+    corresponding test function ``test_{foo}_{solver}`` is then added to the
+    fixture below the class definition.
     """
+
+    @staticmethod
+    def get_test_qpsut01(solver: str):
+        """
+        Get test function for a given solver.
+
+        Parameters
+        ----------
+        solver :
+            Name of the solver to test.
+
+        Returns
+        -------
+        test : function
+            Test function for that solver.
+        """
+
+        def test(self):
+            ref_solution = get_qpsut01()
+            problem = ref_solution.problem
+            solution = solve_problem(problem, solver=solver)
+            eps_abs = (
+                5e-1
+                if solver == "osqp"
+                else 5e-3
+                if solver == "proxqp"
+                else 1e-4
+                if solver == "ecos"
+                else 1e-6
+                if solver in ["cvxopt", "qpswift", "scs"]
+                else 1e-7
+            )
+            self.assertLess(norm(solution.x - ref_solution.x), eps_abs)
+            # NB: in general the dual solution is not unique (that's why the
+            # other tests check residuals). This test only works because the
+            # dual solution is unique in this particular problem.
+            self.assertLess(norm(solution.y - ref_solution.y), eps_abs)
+            self.assertLess(norm(solution.z - ref_solution.z), eps_abs)
+            self.assertLess(norm(solution.z_box - ref_solution.z_box), eps_abs)
+
+        return test
+
+    @staticmethod
+    def get_test_qpsut02(solver: str):
+        """
+        Get test function for a given solver.
+
+        Parameters
+        ----------
+        solver :
+            Name of the solver to test.
+
+        Returns
+        -------
+        test : function
+            Test function for that solver.
+        """
+
+        def test(self):
+            ref_solution = get_qpsut02()
+            problem = ref_solution.problem
+            solution = solve_problem(problem, solver=solver)
+            eps_abs = (
+                5e-2
+                if solver == "ecos"
+                else 5e-4
+                if solver in ["proxqp", "scs"]
+                else 1e-4
+                if solver == "cvxopt"
+                else 1e-5
+                if solver in ["highs", "osqp"]
+                else 5e-7
+                if solver == "qpswift"
+                else 1e-7
+                if solver == "gurobi"
+                else 1e-8
+            )
+            self.assertLess(norm(solution.x - ref_solution.x), eps_abs)
+            self.assertLess(solution.primal_residual(), eps_abs)
+            self.assertLess(solution.dual_residual(), eps_abs)
+            self.assertLess(solution.duality_gap(), eps_abs)
+
+        return test
+
+    @staticmethod
+    def get_test_qpsut03(solver: str):
+        """
+        Get test function for a given solver.
+
+        Parameters
+        ----------
+        solver :
+            Name of the solver to test.
+
+        Returns
+        -------
+        test : function
+            Test function for that solver.
+        """
+
+        def test(self):
+            ref_solution = get_qpsut03()
+            problem = ref_solution.problem
+            solution = solve_problem(problem, solver=solver)
+            self.assertFalse(math.isnan(solution.duality_gap()))
+
+        return test
 
     @staticmethod
     def get_test_maros_meszaros_qptest(solver):
@@ -54,11 +175,55 @@ class TestSolveProblem(unittest.TestCase):
 
         Note
         ----
-        CVXOPT and ECOS fail to solve this problem.
+        ECOS fails to solve this problem.
         """
 
         def test(self):
             solution = get_maros_meszaros_qptest()
+            problem = solution.problem
+            result = solve_problem(problem, solver=solver)
+            tolerance = (
+                1e1
+                if solver == "gurobi"
+                else 1.0
+                if solver == "proxqp"
+                else 2e-3
+                if solver == "osqp"
+                else 5e-5
+                if solver == "scs"
+                else 1e-7
+                if solver == "highs"
+                else 5e-7
+                if solver == "cvxopt"
+                else 1e-8
+            )
+            self.assertIsNotNone(result.x)
+            self.assertIsNotNone(result.z)
+            self.assertIsNotNone(result.z_box)
+            self.assertLess(norm(result.x - solution.x), tolerance)
+            self.assertLess(norm(result.z - solution.z), tolerance)
+            self.assertLess(norm(result.z_box - solution.z_box), tolerance)
+
+        return test
+
+    @staticmethod
+    def get_test_infinite_linear_bounds(solver):
+        """
+        Problem with some infinite linear bounds.
+
+        Parameters
+        ----------
+        solver : string
+            Name of the solver to test.
+
+        Returns
+        -------
+        test : function
+            Test function for that solver.
+        """
+
+        def test(self):
+            solution = get_qpsut01()
             problem = solution.problem
             result = solve_problem(problem, solver=solver)
             tolerance = (
@@ -86,7 +251,22 @@ class TestSolveProblem(unittest.TestCase):
 
 # Generate test fixtures for each solver
 for solver in available_solvers:
-    if solver not in ["cvxopt", "ecos"]:
+    setattr(
+        TestSolveProblem,
+        f"test_qpsut01_{solver}",
+        TestSolveProblem.get_test_qpsut01(solver),
+    )
+    setattr(
+        TestSolveProblem,
+        f"test_qpsut02_{solver}",
+        TestSolveProblem.get_test_qpsut02(solver),
+    )
+    setattr(
+        TestSolveProblem,
+        f"test_qpsut03_{solver}",
+        TestSolveProblem.get_test_qpsut03(solver),
+    )
+    if solver != "ecos":  # ECOS fails to solve this problem
         setattr(
             TestSolveProblem,
             f"test_maros_meszaros_qptest_{solver}",
