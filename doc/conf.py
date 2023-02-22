6ba1bb60ba9b0c92454721de31010a078773039f
Stéphane Caron <stephane.caron@normalesup.org>
2023-02-22 18:40:31 +0100

    [doc] Configure language

--- doc/conf.py
+++ doc/conf.py
@@ -94,7 +94,7 @@ with open(f"{init_path}/__init__.py", "r") as fh:
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
