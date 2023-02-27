4f6760f5c895049fb7baed0611d08141845ce03d
Stéphane Caron <stephane.caron@normalesup.org>
2023-02-27 10:45:01 +0100

    [minor] Add docstrings to all unit test modules

--- tests/test_solution.py
+++ tests/test_solution.py
@@ -18,6 +18,8 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with qpsolvers. If not, see <http://www.gnu.org/licenses/>.
 
+"""Unit tests for Solution class."""
+
 import unittest
 
 import numpy as np
--- tests/test_solve_problem.py
+++ tests/test_solve_problem.py
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with qpsolvers. If not, see <http://www.gnu.org/licenses/>.
 
-"""Tests for the `solve_problem` function."""
+"""Unit tests for the `solve_problem` function."""
 
 import math
 import unittest
