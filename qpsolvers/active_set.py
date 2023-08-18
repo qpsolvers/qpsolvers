c797e952444b8903d62e9cdebab26c9490af9ca6
Stéphane Caron <stephane.caron@normalesup.org>
2023-08-18 15:00:36 +0200

    Lists work better with NumPy arrays
    Which is another way to say tuples don't work ;)

--- qpsolvers/active_set.py
+++ qpsolvers/active_set.py
@@ -21,7 +21,7 @@
 """Active set: indices of inequality constraints saturated at the optimum."""
 
 from dataclasses import dataclass
-from typing import Tuple
+from typing import Sequence
 
 
 @dataclass
@@ -38,16 +38,16 @@ class ActiveSet:
         Indices of active upper-bound inequality constraints.
     """
 
-    G_indices: Tuple[int, ...] = ()
-    lb_indices: Tuple[int, ...] = ()
-    ub_indices: Tuple[int, ...] = ()
+    G_indices: Sequence[int]
+    lb_indices: Sequence[int]
+    ub_indices: Sequence[int]
 
     def __init__(
         self,
-        G_indices: Tuple[int, ...] = (),
-        lb_indices: Tuple[int, ...] = (),
-        ub_indices: Tuple[int, ...] = (),
+        G_indices: Sequence[int] = [],
+        lb_indices: Sequence[int] = [],
+        ub_indices: Sequence[int] = [],
     ) -> None:
-        self.G_indices = tuple(G_indices)
-        self.lb_indices = tuple(lb_indices)
-        self.ub_indices = tuple(ub_indices)
+        self.G_indices = list(G_indices)
+        self.lb_indices = list(lb_indices)
+        self.ub_indices = list(ub_indices)
