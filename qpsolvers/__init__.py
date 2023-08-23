5018775182c9071a9aa011bef1957f95d7ad257c
Stéphane Caron <stephane.caron@normalesup.org>
2023-08-23 15:55:57 +0200

    Release candidate for the upcoming v3.6.0

--- qpsolvers/__init__.py
+++ qpsolvers/__init__.py
@@ -56,7 +56,7 @@ from .solvers import (
 from .unsupported import nppro_solve_qp
 from .utils import print_matrix_vector
 
-__version__ = "3.5.0"
+__version__ = "3.6.0rc1"
 
 __all__ = [
     "ActiveSet",
