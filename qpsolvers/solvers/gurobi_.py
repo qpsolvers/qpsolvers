096795607cf51af20a8ee7ce3926ade660e59d77
Stéphane Caron <stephane.caron@normalesup.org>
2023-12-15 19:24:22 +0100

    Compress source headers by SPDX license identifiers

--- qpsolvers/solvers/gurobi_.py
+++ qpsolvers/solvers/gurobi_.py
@@ -3,21 +3,7 @@
 #
 # Copyright (C) 2016-2022 Stéphane Caron and the qpsolvers contributors.
 # Copyright (C) 2021 Dustin Kenefake
-#
-# This file is part of qpsolvers.
-#
-# qpsolvers is free software: you can redistribute it and/or modify it under
-# the terms of the GNU Lesser General Public License as published by the Free
-# Software Foundation, either version 3 of the License, or (at your option) any
-# later version.
-#
-# qpsolvers is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
-# details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with qpsolvers. If not, see <http://www.gnu.org/licenses/>.
+# SPDX-License-Identifier: LGPL-3.0-or-later
 
 """Solver interface for `Gurobi <https://www.gurobi.com/>`__.
 
