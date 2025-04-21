5f588671c42c7b34c4929ec1268becdc5cfffc81
Stéphane Caron <stephane.caron@inria.fr>
2025-04-21 08:56:21 +0200

    Define qpsolvers-related warnings

---
+++ qpsolvers/warnings.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# SPDX-License-Identifier: LGPL-3.0-or-later
+# Copyright 2025 Inria
+
+"""
+Warnings from qpsolvers.
+"""
+
+
+class QPWarning(UserWarning):
+    """Base class for qpsolvers warnings."""
+
+
+class SparseConversionWarning(QPWarning):
+    """Warning issued when converting NumPy arrays to SciPy sparse matrices."""
