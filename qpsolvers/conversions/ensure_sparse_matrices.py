04f41890dc9e3f24f7ca17b58b2676b3c3bc2794
Stéphane Caron <stephane.caron@inria.fr>
2025-04-21 08:57:07 +0200

    WIP: Mention solver name in sparse conversion warning

--- qpsolvers/conversions/ensure_sparse_matrices.py
+++ qpsolvers/conversions/ensure_sparse_matrices.py
@@ -12,19 +12,23 @@ from typing import Optional, Tuple, Union
 import numpy as np
 import scipy.sparse as spa
 
+from ..warnings import SparseConversionWarning
 
-def __warn_about_sparse_conversion(matrix_name: str) -> None:
+def __warn_about_sparse_conversion(matrix_name: str, solver_name: str) -> None:
     """Warn about conversion from dense to sparse matrix.
 
     Parameters
     ----------
     matrix_name :
         Name of matrix being converted from dense to sparse.
+    solver_name :
+        Name of the QP solver matrices will be passed to.
     """
     warnings.warn(
-        f"Converted {matrix_name} to scipy.sparse.csc.csc_matrix\n"
-        f"For best performance, build {matrix_name} as a "
-        "scipy.sparse.csc_matrix rather than as a numpy.ndarray"
+        f"Converted matrix '{matrix_name}' of your problem to "
+        f"scipy.sparse.csc_matrix to pass it to solver '{solver_name}'; "
+        f"for best performance, build your matrix as a csc_matrix directly.",
+        category=SparseConversionWarning,
     )
 
 
@@ -51,12 +55,12 @@ def ensure_sparse_matrices(
         Tuple of all three matrices as sparse matrices.
     """
     if isinstance(P, np.ndarray):
-        __warn_about_sparse_conversion("P")
+        __warn_about_sparse_conversion("P", solver_name)
         P = spa.csc_matrix(P)
     if isinstance(G, np.ndarray):
-        __warn_about_sparse_conversion("G")
+        __warn_about_sparse_conversion("G", solver_name)
         G = spa.csc_matrix(G)
     if isinstance(A, np.ndarray):
-        __warn_about_sparse_conversion("A")
+        __warn_about_sparse_conversion("A", solver_name)
         A = spa.csc_matrix(A)
     return P, G, A
