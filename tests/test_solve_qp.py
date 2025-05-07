d6a24001b8512a5b86e52d6e6181368a9d0e1705
Stéphane Caron <stephane.caron@inria.fr>
2025-05-07 12:06:22 +0200

    CICD: Update OSQP tolerances in unit tests

--- tests/test_solve_qp.py
+++ tests/test_solve_qp.py
@@ -438,7 +438,11 @@ class TestSolveQP(unittest.TestCase):
             eq_tolerance = (
                 1e-5
                 if solver == "jaxopt_osqp"
-                else 1e-6 if solver in ["qpax", "sip"] else 1e-9
+                else (
+                    1e-6
+                    if solver in ["qpax", "sip"]
+                    else 1e-7 if solver == "osqp" else 1e-9
+                )
             )
             self.assertLess(
                 norm(x - known_solution), sol_tolerance, f"{solver=}"
@@ -485,7 +489,8 @@ class TestSolveQP(unittest.TestCase):
                             if solver in ["proxqp", "sip"]
                             else (
                                 1e-6
-                                if solver in ["cvxopt", "kvxopt", "ecos", "qpax"]
+                                if solver
+                                in ["cvxopt", "kvxopt", "ecos", "qpax"]
                                 else 5e-8 if solver == "qpswift" else 1e-8
                             )
                         )
@@ -498,7 +503,7 @@ class TestSolveQP(unittest.TestCase):
                 else (
                     1e-4
                     if solver in ["qpalm", "qpax", "sip"]
-                    else 5e-10 if solver in ["osqp", "scs"] else 1e-10
+                    else 1e-8 if solver in ["osqp", "scs"] else 1e-10
                 )
             )
             ineq_tolerance = (
@@ -609,7 +614,9 @@ class TestSolveQP(unittest.TestCase):
                 else (
                     5e-6
                     if solver in ["mosek", "proxqp"]
-                    else 1e-7 if solver in ["cvxopt", "kvxopt", "scs"] else 1e-8
+                    else (
+                        1e-7 if solver in ["cvxopt", "kvxopt", "scs"] else 1e-8
+                    )
                 )
             )
             ineq_tolerance = 1e-10
@@ -693,7 +700,9 @@ class TestSolveQP(unittest.TestCase):
                     else 5e-6 if solver in ["proxqp", "qpax", "sip"] else 1e-8
                 )
             )
-            eq_tolerance = 1e-4 if solver in ["jaxopt_osqp", "osqp", "sip"] else 1e-10
+            eq_tolerance = (
+                1e-4 if solver in ["jaxopt_osqp", "osqp", "sip"] else 1e-10
+            )
             ineq_tolerance = (
                 1e-3
                 if solver in ["osqp", "qpalm", "scs"]
@@ -799,7 +808,15 @@ class TestSolveQP(unittest.TestCase):
                         if solver == "proxqp"
                         else (
                             1e-6
-                            if solver in ["cvxopt", "kvxopt", "mosek", "qpswift", "piqp", "sip"]
+                            if solver
+                            in [
+                                "cvxopt",
+                                "kvxopt",
+                                "mosek",
+                                "qpswift",
+                                "piqp",
+                                "sip",
+                            ]
                             else 1e-8
                         )
                     )
